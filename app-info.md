be50b2fe4245c08b21fe77407ed68d9c4ca73490
Jordan Smith <cjordansmith@gmail.com>
2017-02-12 05:08:56 -0600

    transfer copy of app to new repo

--- app-info.md
+++ app-info.md
@@ -5,9 +5,9 @@ uploadr app information
 <input type="filepicker" data-fp-apikey="A5Ty0VK2QjSbaN1SF5mtIz" onchange="alert(event.fpfile.url)">
 
 ** heroku **
-app name:   vast-tor-80986
-app url:    https://vast-tor-80986.herokuapp.com/
-app git:    https://git.heroku.com/vast-tor-80986.git
+app name:   cjs-uploadr
+app url:    https://cjs-uploadr.herokuapp.com/
+app git:    https://git.heroku.com/cjs-uploadr.git
 
 ** mongolab **
 MONGODB_URI: mongodb://heroku_z6dfdq0f:68iqfflk05i534h79hg0kkimru@ds013584.mlab.com:13584/heroku_z6dfdq0f
--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var path = require('path');
 var mongoose = require('mongoose');
-var port = 3000;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var app = express();
-var path = require('path');
+var port = 3000;
 
-var superhero = require('./app/routes/superhero')();
+var app = express();
+
+var routes = require('./app/routes/index')();
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
@@ -14,6 +15,10 @@ mongoose.connect('mongodb://heroku_z6dfdq0f:68iqfflk05i534h79hg0kkimru@ds013584.
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'pug');
 
 //Log with Morgan
 app.use(morgan('dev'));
@@ -27,10 +32,10 @@ app.use(bodyParser.json({ type: 'application/json'}));
 app.use(express.static(__dirname + '/public'));
 
 app.route('/superhero')
-	.post(superhero.post)
-	.get(superhero.getAll);
+	.post(routes.post)
+	.get(routes.getAll);
 app.route('/superhero/:id')
-	.get(superhero.getOne);
+	.get(routes.getOne);
 
 app.listen(port);
 console.log('listening on port ' + port);
